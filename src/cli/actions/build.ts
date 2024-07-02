9a137c66ee6ee97fa759dba889f6cb8ab6534e7e
Anthony Porthouse <anthony.porthouse@bbc.co.uk>
2024-07-02 11:21:18 +0100

    chore: add logging

--- src/cli/actions/build.ts
+++ src/cli/actions/build.ts
@@ -10,26 +10,32 @@ import { getPackFile } from "@/getPackFile.js";
 import { getProjects } from "@/modrinth/getProjects.js";
 import { NormalizedModData, normalizeModData } from "@/normalizeModData.js";
 import { render } from "@/renderer.js";
+import chalk from "chalk";
 import { mkdirSync, writeFileSync } from "fs";
 
 export async function buildAction(
   packPath: Readonly<string>,
   options: Readonly<{ output: string }>,
 ) {
-  const pack = getPackFile(packPath);
+  console.log(`${chalk.blue('Info: ')} Parsing Pack`)
 
+  const pack = getPackFile(packPath);
   const index = getIndexFile(packPath, pack.index.file);
+
+  console.log(`${chalk.blue('Info: ')} Parsing Mod Files`)
   const mods = index.files.map((file) => getModFile(packPath, file.file));
 
   const projects: NormalizedModData[] = [];
 
   try {
+    console.log(`${chalk.blue('Info: ')} Fetching Modrinth Metadata`)
     const modrinthProjects = mods
       .filter(isModrinthFile)
       .map((mod) => mod.update.modrinth["mod-id"]);
 
     const modrinthMods = await getProjects(modrinthProjects);
 
+    console.log(`${chalk.blue('Info: ')} Fetching Curseforge Metadata`)
     const curseforgeModIds = mods
       .filter(isCurseforgeFile)
       .map((mod) => mod.update.curseforge["project-id"]);
@@ -51,9 +57,11 @@ export async function buildAction(
     a.title.localeCompare(b.title),
   );
 
+  console.log(`${chalk.blue('Info: ')} Rendering Template`)
+
   const output = render(pack, sortedProjects);
 
+  console.log(`${chalk.blue('Info: ')} Outputting to ${options.output}/index.html`)
   mkdirSync(options.output, { recursive: true });
-
   writeFileSync(`${options.output}/index.html`, output);
 }
